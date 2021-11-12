d4dd56a4d0a8390bd5369c17793831966d64c6de
Victor Souza <victor.augusto800@gmail.com>
2021-11-12 19:17:12 -0300

    feat:Add message for no matches on pokemon search

--- src/components/ListPokemon.js
+++ src/components/ListPokemon.js
@@ -16,7 +16,18 @@ const ListPokemon = () => {
           arrayPokemon.push(dataPokemonOrder[i + (currentPage - 1) * 21]);
         }
       }
-
+      if (arrayPokemon.length === 0) {
+        return (
+          <div>
+            <h3 style={{ color: "#a1291b", fontWeight: "bold" }}>
+              No Pokémon matched your search!
+            </h3>
+            <p style={{ fontWeight: "bold" }}>
+              Make sure you typed the correct name or number of the Pokémon!
+            </p>
+          </div>
+        );
+      }
       return arrayPokemon.map((pokemon) => {
         const id = pokemon.entry_number;
         const url = `https://pokeapi.co/api/v2/pokemon/${id}/`;
